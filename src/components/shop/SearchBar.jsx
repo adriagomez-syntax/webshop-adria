be54ef5fe8a06584ebb069e47bd9a347b4a0b13b
Adria Gomez <adria.gomez@tn.syntax-institut.de>
2026-08-26 18:54:56 +0200

    Clear search button

--- src/components/shop/SearchBar.jsx
+++ src/components/shop/SearchBar.jsx
@@ -1,18 +1,29 @@
 import { FaSearch } from "react-icons/fa"
 import Input from "../common/Input"
+import { FaX } from "react-icons/fa6";
+import Button from "../common/Button";
 
 export default function SearchBar({ searchValue, setSearch }) {
-	
-	
+
+	function clearSearch(){
+		setSearch("");
+	}
+
 	return (
 		<div className="flex gap-0 w-full">
 			<div className="flex justify-center items-center border-l border-y border-secondary px-2 py-2 rounded-l-md">
 				<FaSearch className="text-center" />
 			</div>
-			<Input className="w-full" border="border border-secondary rounded-r-md"
+			<Input className="w-full" border="border-y border-secondary"
 				placeholder="Suchen..."
 				onChange={ (event) => setSearch(event.target.value) }
-				value={ searchValue } />
+				value={ searchValue } 
+			/>
+			<div className="flex justify-center items-center border-r border-y border-secondary px-2 py-2 rounded-r-md">
+				<Button onClick={ clearSearch }>
+					{searchValue !== "" && <FaX className="text-2xs text-accent-1/50" />}
+				</Button>
+			</div>
 		</div>
 	)
 }
