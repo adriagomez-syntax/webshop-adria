a0cd10f916fe81787fe21a9f87903cb575117ee1
Adria Gomez <adria.gomez@tn.syntax-institut.de>
2026-09-01 14:02:10 +0200

    Fixed default checked input radio

--- src/components/common/InputRadio.jsx
+++ src/components/common/InputRadio.jsx
@@ -1,10 +1,10 @@
 import Button from "./button/Button";
 
-export default function InputRadio({ id, name, label, onChange, value }) {
+export default function InputRadio({ id, name, label, onChange, value, checked }) {
 	return (
 		<Button>
 			<label className="flex gap-2 items-center group">
-				<input id={ id } type="radio" name={ name } onChange={ onChange } value={ value } className="peer hidden" />
+				<input id={ id } type="radio" name={ name } onChange={ onChange } value={ value } checked={ checked } className="peer hidden" />
 				<div className="w-3 h-3 flex items-center justify-center rounded-full border border-background-card peer-checked:border-secondary transition ease-out duration-300">
 					<div className="w-1 h-1 rounded-full bg-secondary opacity-0 group-has-[input:checked]:opacity-100 transition ease-out duration-300"></div>
 				</div>
