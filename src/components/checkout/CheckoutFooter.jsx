4584699ee6ed809c01c64873beea1e72fbc44fea
Adria Gomez <adria.gomez@tn.syntax-institut.de>
2026-09-03 14:05:07 +0200

    Fixed checkout footer display

--- src/components/checkout/CheckoutFooter.jsx
+++ src/components/checkout/CheckoutFooter.jsx
@@ -1,11 +1,14 @@
 import { FaArrowLeft } from "react-icons/fa";
 import { Link } from "react-router";
+import Section from "../common/Section";
 
 export default function CheckoutFooter() {
 	return (
-		<Link to="/cart" className="flex gap-2 items-center text-secondary hover:underline">
-			<FaArrowLeft />
-			<p>Zurück zum Warenkorb</p>
-		</Link>
+		<Section>
+			<Link to="/cart" className="flex gap-2 items-center text-secondary hover:underline">
+				<FaArrowLeft />
+				<p>Zurück zum Warenkorb</p>
+			</Link>
+		</Section>
 	)
 }
